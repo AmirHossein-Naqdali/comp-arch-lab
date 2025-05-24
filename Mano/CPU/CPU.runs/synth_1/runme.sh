0ef63c797c75edb29b93e87b8f4d281885227fec
AmirHossein <amirhossein.n2003@gmail.com>
2025-05-24 09:43:03 +0330

    add unimportant files

--- Mano/CPU/CPU.runs/synth_1/runme.sh
+++ Mano/CPU/CPU.runs/synth_1/runme.sh
@@ -6,10 +6,14 @@
 # Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 # 
 
+echo "This script was generated under a different operating system."
+echo "Please update the PATH and LD_LIBRARY_PATH variables below, before executing this script"
+exit
+
 if [ -z "$PATH" ]; then
-  PATH=/home/eftekhar/Applications/Xilinx/Vivado/2022.2/ids_lite/ISE/bin/lin64:/home/eftekhar/Applications/Xilinx/Vivado/2022.2/bin
+  PATH=C:/Xilinx/Vivado/2022.2/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2022.2/ids_lite/ISE/lib/nt64:C:/Xilinx/Vivado/2022.2/bin
 else
-  PATH=/home/eftekhar/Applications/Xilinx/Vivado/2022.2/ids_lite/ISE/bin/lin64:/home/eftekhar/Applications/Xilinx/Vivado/2022.2/bin:$PATH
+  PATH=C:/Xilinx/Vivado/2022.2/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2022.2/ids_lite/ISE/lib/nt64:C:/Xilinx/Vivado/2022.2/bin:$PATH
 fi
 export PATH
 
@@ -20,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='/home/eftekhar/programs/sakht/files/Mano/CPU/CPU.runs/synth_1'
+HD_PWD='C:/Users/SSP-1234/Desktop/CPU/CPU/CPU.runs/synth_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
@@ -36,4 +40,4 @@ EAStep()
      fi
 }
 
-EAStep vivado -log Memory.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source Memory.tcl
+EAStep vivado -log FPGA.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source FPGA.tcl
